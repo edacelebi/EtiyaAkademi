ccdccafc0a68cef8d2459c04dd0825431f8468b3
Eda Çelebi (ETIYA) <eda.celebi@etiya.com>
2023-09-30 21:28:10 +0300

    ProgrammingLanguage chapter4

---
+++ ProgrammingLanguages/src/main/java/com/example/programminglanguages/dataAccess/abstracts/ProgrammingLanguageRepository.java
@@ -0,0 +1,15 @@
+package com.example.programminglanguages.dataAccess.abstracts;
+
+import com.example.programminglanguages.entities.concretes.ProgrammingLanguage;
+
+import java.util.List;
+
+public interface ProgrammingLanguageRepository {
+
+    void add(ProgrammingLanguage programmingLanguage);
+    void delete(int id) throws Exception;
+    void update(int id ,ProgrammingLanguage programmingLanguage) throws Exception;
+    List<ProgrammingLanguage>getAll();
+   ProgrammingLanguage getById(int id);
+
+}
